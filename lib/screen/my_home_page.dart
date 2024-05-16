e3f68642ceeb0e87cb4e16f8648e0905a76fcf9c
Kullasup Phongsrisuk <phongsrisuk@gmail.com>
2024-05-16 16:03:31 +0700

    Finish

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,4 +1,4 @@
-
+import 'package:example/screen/my_second_page.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -6,6 +6,28 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello World");
+    return Scaffold(
+      drawer: const Drawer(
+        child: Text("My drawer"),
+      ),
+      appBar: AppBar(title: const Text("My Home Page")),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Hello World"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const MySecondPage(),
+                  ),
+                );
+              },
+              child: const Text("ไปยังหน้าสอง"),
+              ),
+          ],
+        )
+      ),
+    );
   }
 }
